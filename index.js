8e5da3b3ea71aa402bb30a8f943bf88a1b75bbe5
ententapl2 <maksikx08@onet.pl>
2025-09-24 07:12:54 +0000

    use pageHandlers.js in index.js server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { createServer } from 'node:http';
 import { URL } from 'node:url'
-import {root, hello} from './pageHandlers.js'
+import {root, hello, methodNotAllowed, notFound} from './pageHandlers.js'
 
 const port = 8000;
 const host = 'localhost';
@@ -19,16 +19,19 @@ const pathConf = [
 const server = createServer((req, res) => {
 
 	const url = new URL(`http://${host}${req.url}`);
-	const path = url.pathname
-	const method = req.method 
+	const path = url.pathname;
+	const method = req.method;
 	
+	for (let conf of pathConf) {
 
-	if (!res.writableEnded) {
-		
-		res.writeHead(404, {'Content-Type': 'text/plain'});
-		res.end('Page not found!');
+		if (conf.path === path) {
+			conf.allowed_methods.includes(method) ? conf.handler(req, res) : methodNotAllowed(req, res);
+			break;
+		} 
 
 	}
+	!res.writableEnded && notFound(req, res);
+
 
 });
 
--- pageHandlers.js
+++ pageHandlers.js
@@ -10,3 +10,15 @@ export function hello(req, res) {
     res.writeHead(200, {'Content-Type':'text/plain'});
     res.end("Hello world\n");
 }
+
+export function notFound(req, res) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Page not found!');
+}
+
+export function methodNotAllowed(req, res) {
+
+    res.writeHead(405, {'Content-Type': 'text/plain'});
+    res.end('Method not allowed!');
+
+}
