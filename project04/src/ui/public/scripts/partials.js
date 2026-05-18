2a141ca5e6e4c6d203b84a94f80d0fc4e9f4fcf3
ententapl2 <maksikx08@onet.pl>
2026-05-18 08:35:36 +0000

    ADD THEMES

--- project04/src/ui/public/scripts/partials.js
+++ project04/src/ui/public/scripts/partials.js
@@ -6,6 +6,7 @@ function handleSearchBarToggle() {
     const headerIcon = nav.querySelector("#headerIcon");
     const userIcon = nav.querySelector("#userIconButton");
     const logoutIcon = nav.querySelector("#logoutIconButton");
+    const themeIcon = nav.querySelector("#toggleTheme");
 
     if (searchBar.classList.contains('active')) {
         const searchBarContent = (searchBar.querySelector("input").value ?? '').trim();
@@ -18,12 +19,37 @@ function handleSearchBarToggle() {
         headerIcon.classList.add('active');
         userIcon.classList.add('active');
         logoutIcon?.classList?.add('active');
+        themeIcon?.classList.add('active');
     } else {
         searchBar.classList.add('active');
         headerIcon.classList.remove('active');
         userIcon.classList.remove('active');
         logoutIcon?.classList?.remove('active');
+        themeIcon?.classList.remove('active');
     }
+}
+
+
+function applyTheme() {
+
+    const stored = localStorage.getItem('theme');
+    const system = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const theme = stored ?? (system ? 'dark' : 'light');
+
+
+    document.querySelector('#toggleTheme').src = '/images/icons/' + (theme === 'dark' ? 'light' : 'dark') + '.svg';
+    document.documentElement.setAttribute('data-theme', theme);
+
+}
+
+function handleThemeToggle() {
+
+    const isDarkCurrent = document.documentElement.getAttribute('data-theme') === 'dark';
+    const theme = isDarkCurrent ? 'light' : 'dark';
+    localStorage.setItem('theme', theme);
+    applyTheme();
+    
+    
 }
 
 function handleSubmit(e) {
@@ -36,3 +62,5 @@ function handleSubmit(e) {
 
 document.querySelector("#searchIconButton").addEventListener('click', handleSearchBarToggle);
 document.querySelector(".__headerSearchBar input").addEventListener('keydown', handleSubmit);
+document.querySelector('#toggleTheme').addEventListener('click', handleThemeToggle);
+applyTheme();
